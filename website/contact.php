731f8372d49b1658be9e8de95a3a6b2006326b33
jessalbarian <albarian.jess@gmail.com>
2016-08-02 14:05:15 -0600

    more form stuff

--- website/contact.php
+++ website/contact.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en-US">
     <?php include('head.php'); ?>
-    
+
     <body data-spy="scroll" data-target="#main-navbar">
         <div class="page-loader"></div>  <!-- Display loading image while page loads -->
         <div class="body">
@@ -10,19 +10,36 @@
             <section id="text-carousel-intro-section" class="" data-stellar-background-ratio="0.5"
                      style="background-image: url(img/maincover2b.png); background-size: cover; background-repeat: no-repeat;background-attachment: fixed;
                      -webkit-background-size: cover;">
-                <div class="container" id="main">
-                    <div class="caption text-center" data-stellar-ratio="0.7">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div id="form_sample"></div>
+                <div class="container" >
+                        <div id="form-main">
+                            <div id="form-div">
+                                <form class="form" id="form1">
+
+                                    <p class="name">
+                                        <input name="name" type="text" class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="Name" id="name" />
+                                    </p>
+
+                                    <p class="email">
+                                        <input name="email" type="text" class="validate[required,custom[email]] feedback-input" id="email" placeholder="Email" />
+                                    </p>
+
+                                    <p class="text">
+                                        <textarea name="text" class="validate[required,length[6,300]] feedback-input" id="comment" placeholder="Comment"></textarea>
+                                    </p>
+
+
+                                    <div class="submit">
+                                        <input type="submit" value="SEND" id="button-blue"/>
+                                        <div class="ease"></div>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
-                </div>
             </section>
-            
+
             <?php include('footer.php'); ?>
-            
+
             <?php include('includes.php'); ?>
     </body>
 
