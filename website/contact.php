feb670f5f2383070c875666f6a2ae2c4b7c7700c
jessalbarian <albarian.jess@gmail.com>
2016-08-02 13:54:01 -0600

    edited form

--- website/contact.php
+++ website/contact.php
@@ -10,7 +10,7 @@
             <section id="text-carousel-intro-section" class="" data-stellar-background-ratio="0.5"
                      style="background-image: url(img/maincover2b.png); background-size: cover; background-repeat: no-repeat;background-attachment: fixed;
                      -webkit-background-size: cover;">
-                <div id="main" class="container">
+                <div class="container" id="main">
                     <div class="caption text-center" data-stellar-ratio="0.7">
                         <div class="row">
                             <div class="col-lg-12">
