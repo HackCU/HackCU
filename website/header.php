4c7c8553bfe2d38f823937d88d3b76df45ad8b1b
jessalbarian <albarian.jess@gmail.com>
2016-08-02 21:51:01 -0600

    updated htaccess

--- website/header.php
+++ website/header.php
@@ -16,7 +16,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand page-scroll" href="index.php">HackCU</a>
+                <a class="navbar-brand page-scroll" href="index">HackCU</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
