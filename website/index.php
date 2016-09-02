c3072e275748fa2f6930a808df609d1b4e3fcc14
jessalbarian <albarian.jess@gmail.com>
2016-09-02 10:54:26 -0600

    added pic

--- website/index.php
+++ website/index.php
@@ -607,22 +607,22 @@
                                     </div>
                                 </div>
 
-                                <!--Shubha Swamy-->
-                                <!--<div class="col-md-2">-->
-                                    <!--<div class="team-container wow bounceIn" data-wow-delay="0.8s">-->
-                                        <!--<div class="team-item">-->
-                                            <!--<div class="team-triangle">-->
-                                                <!--<div class="content">-->
-                                                    <!--<img src="img/organizers/shubha.jpg" alt="Shubha Swamy"/>-->
-                                                    <!--<div class="team-hover text-center">-->
-                                                        <!--<i class="fa fa-gears"></i>-->
-                                                        <!--<p>Shubha Swamy</p>-->
-                                                    <!--</div>-->
-                                                <!--</div>-->
-                                            <!--</div>-->
-                                        <!--</div>-->
-                                    <!--</div>-->
-                                <!--</div>-->
+                                 <!--Shubha Swamy-->                                
+                                 <div class="col-md-2">
+                                    <div class="team-container wow bounceIn" data-wow-delay="0.8s">
+                                        <div class="team-item">
+                                            <div class="team-triangle">
+                                                <div class="content">
+                                                    <img src="img/organizers/shubha.jpg" alt="Shubha Swamy"/>
+                                                    <div class="team-hover text-center">
+                                                        <i class="fa fa-gears"></i>
+                                                        <a href="https://shubhaswamy.github.io/" target="_blank"><p>Shubha Swamy</p></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
 
                             </div>
                         </div>
