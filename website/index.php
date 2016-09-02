9eca81053d4689bb96dffc1ff55b7ec64ad7ca55
jessalbarian <albarian.jess@gmail.com>
2016-09-02 08:06:41 -0600

    added mlh

--- website/index.php
+++ website/index.php
@@ -362,19 +362,16 @@
                                 <div class="col-lg-12">
                                     <img src="img/partners/spark.png">
                                 </div>
-<!--                                <div class="col-lg-6">-->
-<!--                                    <img src="img/partners/mlh-logo.png">-->
-<!--                                </div>-->
                             </div>
                             <div class="row">
-                                <!--<div class="col-lg-4">-->
-                                    <!--<img src="img/partners/engineering.png">-->
+                                <div class="col-lg-4">
+                                    <img src="img/partners/mlh-logo.png">
 
-                                <!--</div>-->
-                                <div class="col-lg-6">
+                                </div>
+                                <div class="col-lg-4">
                                     <img src="img/partners/kind.png">
                                 </div>
-                                <div class="col-lg-6">
+                                <div class="col-lg-4">
                                     <img src="img/partners/leeds.png">
                                 </div>
                             </div>
