06cacaba4ac0bbcc0aac018f64bbc09df2892c22
jessalbarian <albarian.jess@gmail.com>
2016-09-20 11:05:09 -0600

    added payment page

--- website/apply.php
+++ website/apply.php
@@ -11,10 +11,10 @@
                      style="background-image: url(img/maincover2b.png); background-size: cover; background-repeat: no-repeat;background-attachment: fixed;
                      -webkit-background-size: cover;">
 			<!-- Change the width and height values to suit you best -->
-			<div class="typeform-widget" data-url="https://hackcu.typeform.com/to/Vsm9Y9" data-text="Local Hack Day 2016" style="width:100%;height:500px;"></div>
-			<script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'widget.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()</script>
-			<div style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;">Powered by <a href="https://www.typeform.com/examples/forms/?utm_campaign=Vsm9Y9&amp;utm_source=typeform.com-1163440-Basic&amp;utm_medium=typeform&amp;utm_content=typeform-embedded-poweredbytypeform&amp;utm_term=EN" style="color: #999" target="_blank">Typeform</a></div>
-            </section>
+<!--			<div class="typeform-widget" data-url="https://hackcu.typeform.com/to/Vsm9Y9" data-text="Local Hack Day 2016" style="width:100%;height:500px;"></div>-->
+<!--			<script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'widget.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()</script>-->
+<!--			<div style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;">Powered by <a href="https://www.typeform.com/examples/forms/?utm_campaign=Vsm9Y9&amp;utm_source=typeform.com-1163440-Basic&amp;utm_medium=typeform&amp;utm_content=typeform-embedded-poweredbytypeform&amp;utm_term=EN" style="color: #999" target="_blank">Typeform</a></div>-->
+<!--            </section>-->
 
             <?php include('footer.php'); ?>
 
