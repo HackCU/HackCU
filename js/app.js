d27551cae854e36e621ff82435f649579cd2ff83
Cathal Killeen <cathalkilleen@gmail.com>
2016-10-22 17:47:55 -0600

    added theme and split sections into different files

--- js/app.js
+++ js/app.js
@@ -3,3 +3,7 @@ var app = angular.module('hackcu2017', [])
 .controller('AboutCtrl', function($scope){
     $scope.about = "About Us"; //just testing
 })
+
+.controller('HeaderCtrl', function($scope){
+    $scope.about = "About Us"; //just testing
+})
