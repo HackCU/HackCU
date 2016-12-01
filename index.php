de85c4a2fa3e9e9619b834448fcad2322edccbdf
Apara <ssapar@gmail.com>
2016-11-30 18:52:14 -0700

    Correct jQuery script src

--- index.php
+++ index.php
@@ -246,7 +246,7 @@
 
 </script>
 <!-- jQuery -->
-<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="js/jquery.min.js"></script>
 
 <!-- Bootstrap Core JavaScript -->
 <script src="js/bootstrap.min.js"></script>
