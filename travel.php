568484eadc66c4f651c682da1585ad45f8cd950d
Jessie Albarian <albarian.jess@gmail.com>
2016-12-30 17:48:44 +0100

    fixed faq and added travel page

---
+++ travel.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>HackCU Travel</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Custom Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="bower_components/simple-line-icons/css/simple-line-icons.css">
+
+    <!-- Theme & Custom CSS -->
+    <link href="css/styles.css" rel="stylesheet">
+    <link href="css/new-age.css" rel="stylesheet">
+
+
+</head>
+
+<body id="page-top" ng-app="hackcu2017">
+
+<nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand page-scroll" id="brand-text" href="#page-top">HackCU 2017</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a class="page-scroll" href="index.php/#about">About</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="index.php/#apply">Apply</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="index.php/#schedule">Schedule</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="index.php/#faq">FAQ</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="index.php/#location">Location</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="index.php/#sponsors">Sponsors</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="index.php/#team">Team</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="index.php/#contact">Contact</a>
+                </li>
+                <!--
+                    <li>
+                    <a id="mlh-trust-badge" style="display:block;max-width:100px;min-width:60px;position:fixed;width:10%;z-index:10000;" href="https://mlh.io/seasons/na-2017/events?utm_source=na-2017&utm_medium=TrustBadge&utm_campaign=na-2017&utm_content=white" target="_blank"><img src="https://s3.amazonaws.com/logged-assets/trust-badge/2017/white.svg" alt="Major League Hacking 2017 Hackathon Season" style="width:100%"></a>
+                    </li>
+                -->
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+
+    </div>
+    <!-- /.container-fluid -->
+</nav>
+
+<header>
+
+
+    <!--            <div class="keyart" id="nonparallax">-->
+    <div id="travel" ng-controller="HeaderCtrl">
+        <div class="row">
+            <div class="col-md-12 container">
+                <div class="header-content" style="z-index: 100;">
+                    <div class="header-content-inner text-center location" style="bottom: 0;">
+<!--                        <img src="images/hackculogo.png" alt="logo" />-->
+                        <h1>HackCU 2017 Travel</h1>
+                        <p class="smallMar">How do you get to HackCU?</p>
+<!--                        <p class="smallMar">April 8<sup>th</sup> to April 9<sup>th</sup> 2017</p>-->
+<!--                        <p>University of Colorado Boulder</p>-->
+
+                        <div class="row largeMar">
+                            <div class="col-lg-4">
+                                <h2>Location and Parking</h2>
+                                <p>Check-in will be in the Math building in room MATH 100. This is located at:
+                                    <b><br>184 UCB
+                                    <br>Boulder, CO 80309</b>
+                                    <br>The rest of the event
+                                    will be located in the Wolf Law building at:
+                                    <br><b>2450 Kittredge Loop Dr.
+                                        <br>Boulder, CO 80305</b></p>
+                                <p>Parking will be available in the parking lot outside of Wolf Law on Kittredge Loop Dr. Be sure to follow directions on the signs to make sure you are not in permit only parking.</p>
+                            </div>
+
+                            <div class="col-lg-4">
+                                <h2>Coming From the Airport</h2>
+                                <p>If you take an Uber or Lyft from DIA (Denver International Airport), use the code HACKCU to get a $5 discount! Save your receipts to be used for your travel reimbursement if you were approved.</p>
+                                <p>There is a bus, the RTD AB line, that runs from DIA to our university campus. Get off at the Euclid and Broadway stop.</p>
+
+                            </div>
+
+                            <div class="col-lg-4">
+                                <h2>Grant Amounts</h2>
+                                <p>If you are traveling farther then 25 miles from the University of Colorado Boulder, you are eligible for travel reimbursement. To receive reimbursement, you must:</p>
+                                <ol>
+                                    <li>RSVP to the link via email.</li>
+                                    <li>Be approved by the HackCU admin team for reimbursement. You will receive an email.</li>
+                                    <li>Provide receipts to the travel reimbursement station on Sunday at the event.</li>
+                                    <li>Submit a project at the end of the hackathon.</li>
+                                </ol>
+                            </div>
+
+
+                        </div>
+                        <div class="row largeMar">
+                            <div class="col-lg-4">
+                                <h2>Flights</h2>
+                                <p>We will be offering flight reimbursements on a first come first serve basis with the amount reimbursed affected by distance traveled. Once approved by our admin team via email, you will
+                                    be required to submit travel receipts at our travel station on Sunday at the event. Your entire flight may or may not be covered by reimbursements.</p>
+
+                            </div>
+                            <div class="col-lg-4">
+                                <h2>International Travel</h2>
+                                <p>We love international participants! Unfortunately, HackCU cannot provide assistance for international travel or visa paperwork. However, if you do make
+                                    it to HackCU, send us an email at contact[at]hackcu.org with the subject line: "International Participant" and we can put in a request for reimbursement.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <!--            Shows nonparallax image-->
+<!--            <div class="layer layer-07"></div>-->
+        </div>
+    </div>
+</header>
+
+
+
+<!--<section id="about" class="text-center about">-->
+<!--    --><?php //include('sections/about.html'); ?>
+<!--</section>-->
+<!---->
+<!--<section id="apply" class="">-->
+<!--    --><?php //include('sections/apply.html'); ?>
+<!--</section>-->
+<!---->
+<!--<section id="goals" class="faq">-->
+<!--    --><?php //include('sections/goals.html'); ?>
+<!--</section>-->
+<!---->
+<!--<section id="schedule" class="text-center">-->
+<!--    --><?php //include('sections/schedule.html'); ?>
+<!--</section>-->
+<!---->
+<!---->
+<!--<section id="faq" class="faq">-->
+<!--    --><?php //include('sections/faq.html'); ?>
+<!--</section>-->
+<!---->
+<!--<section id="location" class="text-center">-->
+<!--    --><?php //include('sections/location.html'); ?>
+<!--</section>-->
+<!---->
+<!--<section id="sponsors" class="text-center">-->
+<!--    --><?php //include('sections/sponsors.html'); ?>
+<!--</section>-->
+<!---->
+<!--<section id="team" class="text-center">-->
+<!--    --><?php //include('sections/team.html'); ?>
+<!--</section>-->
+<!---->
+<!--<section id="contact" class="contact">-->
+<!--    --><?php //include('sections/contact.html'); ?>
+<!--</section>-->
+
+<footer>
+    <div class="container">
+
+        <p>Made with <i class="fa fa-heart"></i> by HackCU</p>
+        <div class="row">
+            <div class="col-lg-4">
+                <ul>
+                    <li>
+                        <a href="application.html" target="_blank">Apply</a>
+                    </li>
+                    <li>
+                        <a href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf" target="_blank">MLH Code</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-4">
+            </div>
+            <div class="col-lg-4">
+                <ul>
+                    <li>
+                        <a href="https://localday2016.hackcu.org/" target="_blank" >Local Hack Day 2016</a>
+                    </li>
+                    <li>
+                        <a href="https://2016.hackcu.org/" target="_blank">HackCU 2016</a>
+                    </li>
+                    <li>
+                        <a href="https://localday2015.hackcu.org/" target="_blank" >Local Hack Day 2015</a>
+                    </li>
+                    <li>
+                        <a href="https://startups2students.hackcu.org/" target="_blank">Startup2Students</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- jQuery -->
+<script src="js/jquery.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+<!-- Angular Core JavaScript -->
+<script src="bower_components/angular/angular.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+<!-- Theme JavaScript -->
+<script src="js/new-age.js"></script>
+
+<!-- Angular App JS -->
+<script src="js/app.js"></script>
+
+<!-- External JS -->
+<script src="js/waypoints.min.js"></script>
+<script src="js/jquery.counterup.min.js"></script>
+
+
+</body>
+
+</html>
