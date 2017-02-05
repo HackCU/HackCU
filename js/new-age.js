407a9e3bea43478e7cda860adbd9ed16c732fa3c
Cathal Killeen <cathalkilleen@gmail.com>
2017-02-04 20:27:04 -0700

    fixed smooth scrolling :D

--- js/new-age.js
+++ js/new-age.js
@@ -5,7 +5,7 @@
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            scrollTop: ($(this.hash).offset().top - 50)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
