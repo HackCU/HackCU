e672ab0829c0c63c41d3cb3212af5d6c62f1b918
Apara <ssapar@gmail.com>
2017-04-21 16:27:40 -0600

    Change limit of team size to 4

--- live/index.php
+++ live/index.php
@@ -224,7 +224,7 @@
                     <p>
                     <ol>
                         <li>
-                            There is no maximum or minimum team size. As a participant, you should make sure to check how many prizes are available per team. There is usually a limited number of prizes for each challenge. So if you form a large team and win a challenge, there might not be enough prizes for everyone on your team.
+                            The maximum number of hackers per team is limited to 4. As a participant, you should make sure to check how many prizes are available per team. There is usually a limited number of prizes for each challenge. So if you form a large team and win a challenge, there might not be enough prizes for everyone on your team.
                         </li>
                         <li>
                             Teams should be made up exclusively of students (or recent graduates within one year of having graduated) who are not organizers, volunteers, judges, sponsors, or in any other privileged position at the event.
