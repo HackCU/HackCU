9f5bad818d3fe3627d2c1712566ba499886ed10e
Gerard Casas Saez <casassg@gmail.com>
2018-11-25 12:34:44 -0700

    reduce a bit load and improve accessibility

--- assets/js/flakes.js
+++ assets/js/flakes.js
@@ -4,28 +4,18 @@
 
 
 $(function () {
-  var renderer = PIXI.autoDetectRenderer(1920, 1080, { antialias: true, transparent: true });
-  renderer.view.style.width = "100%";
-  renderer.view.style.height = "100%";
+  var renderer = PIXI.autoDetectRenderer(1920, 1080, { transparent: true });
   document.body.insertBefore(renderer.view, document.body.childNodes[0]);
 
   var stage = new PIXI.Container();
   var startTime = null;
   var angle = 0;
+
   // Init sprites
   var circle = createCircleSprite();
 
   // Constants
   var numFlakes = 300;
-
-  // Init flakes
-  var flakes = [];
-  for (var i = 0; i < numFlakes; i++) {
-    var f = createFlake();
-    flakes.push(f);
-    stage.addChild(f);
-  }
-  renderer.render(stage);
 
 
   function update(dt) {
@@ -38,27 +28,26 @@ $(function () {
     for (var i = 0; i < numFlakes; i++) {
       var flake = flakes[i];
 
-      if (flake != undefined) {
-        flake.y += (Math.cos(angle + flake.weight) + 1 + flake.width / 25) * 22 * dt;
-        flake.x += sinInc;
+      flake.y += (Math.cos(angle + flake.weight) + 1 + flake.width / 25) * 22 * dt;
+      flake.x +=  Math.sin(angle) * 50 * dt / 2;
 
-        // Sending flakes back from the top when it exits
-        // Lets make it a bit more organic and let flakes enter from the left and right also.
-        if (flake.x > W + 5 || flake.x < -5 || flake.y > H) {
-          if (i % 20 > 1) /* 90% of the flakes */ {
-            flakes[i].x = Math.random() * W;
-            flakes[i].y = - 5;
+      // Sending flakes back from the top when it exits
+      // Lets make it a bit more organic and let flakes enter from the left and right also.
+      if (flake.x > W + 5 || flake.x < -5 || flake.y > H) {
+        if (i % 20 > 1) /* 90% of the flakes */ {
+          flake.x = Math.random() * W;
+          flake.y = - 5;
+        } else {
+          // If the flake is exitting from the right
+          if (Math.sin(angle) > 0) {
+            flake.x = -5;
+            flake.y = Math.random() * H;
           } else {
-            // If the flake is exitting from the right
-            if (Math.sin(angle) > 0) {
-              flakes[i].x = -5;
-              flakes[i].y = Math.random() * H;
-            } else {
-              flakes[i].x = W + 5;
-              flakes[i].y = Math.random() * H;
-            }
+            flake.x = W + 5;
+            flake.y = Math.random() * H;
           }
         }
+
       }
     }
   }
@@ -66,7 +55,7 @@ $(function () {
 
   function mainLoop(timestamp) {
 
-    requestAnimationFrame(function (ts) { mainLoop(ts) });
+    requestAnimationFrame(mainLoop);
 
     if (startTime == null) startTime = timestamp;
     var dt = (timestamp - startTime) / 700;
@@ -94,13 +83,13 @@ $(function () {
   // Element constructors
   function createFlake() {
     var f = new PIXI.Sprite(circle);
-    var scale = (Math.random() * 1.5) + 0.8;
-    f.position.x = Math.random() * renderer.width;
-    f.position.y = Math.random() * renderer.height;
-    f.alpha = Math.random();
+    var scale = (Math.random() * 0.7) + 0.3;
+    f.position.x = Math.random() * renderer.view.width;
+    f.position.y = Math.random() * renderer.view.height;
+    f.alpha = 0.9;
     f.weight = Math.random() * 20;
-    f.scale.x = scale;
-    f.scale.y = scale;
+    f.scale.x = scale*2;
+    f.scale.y = scale*2;
     return f;
   }
 
@@ -115,7 +104,15 @@ $(function () {
   window.addEventListener('resize', resize, false)
   resize()
 
-  requestAnimationFrame(function (ts) { mainLoop(ts) });
+  // Init flakes
+  var flakes = [];
+  for (var i = 0; i < numFlakes; i++) {
+    var f = createFlake();
+    flakes.push(f);
+    stage.addChild(f);
+  }
+  renderer.render(stage);
+  requestAnimationFrame(mainLoop);
 
 
 })
