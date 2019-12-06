91d75379de35507218e1e5c39d670432073b8efb
MattStrong <mast4878@colorado.edu>
2019-12-06 08:58:28 -0700

    correct math

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -4,15 +4,11 @@ var box = document.getElementById("mydiv");
 var box1 = document.getElementById("blank_window_1");
 var box2 = document.getElementById("blank_window_2");
 var box3 = document.getElementById("blank_window_3")
-// var box4 = document.getElementById("about_tab");
-// var box5 = document.getElementById("schedule_tab");
 
 box.style.position = "absolute";
 box1.style.position = "absolute";
 box2.style.position = "absolute";
 box3.style.position = "absolute";
-// box4.style.position = "relative"; 
-// box5.style.position = "relative"; 
 
 
 box.style.zIndex = 100;
@@ -21,7 +17,7 @@ box2.style.zIndex = 98;
 box3.style.zIndex = 97;
 
 var init_top = 150;
-var init_left = screen.width/3.8;
+var init_left = screen.width/2 - 400;
 
 
 box1.style.top = String(init_top - 20) + "px";
