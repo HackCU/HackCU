cb17e0f908fc9fd00ca4068b46971ddda033f978
MattStrong <mast4878@colorado.edu>
2019-12-07 18:46:18 -0700

    fix

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -26,10 +26,38 @@ box2.style.top = String(init_top - 40) + "px";
 box2.style.left = String(init_left - 40) + "px";
 box3.style.top = String(init_top - 60) + "px";
 box3.style.left = String(init_left - 60) + "px";
-console.log(String(init_left - 60) + "px");
 box.style.top = String(init_top) + "px";
 box.style.left = String(init_left) + "px";
 
+
+var onsize = function() {
+    var width = $(window).width();
+    var box = document.getElementById("mydiv");
+    var box1 = document.getElementById("blank_window_1");
+    var box2 = document.getElementById("blank_window_2");
+    var box3 = document.getElementById("blank_window_3")
+    box.style.zIndex = 100;
+    box1.style.zIndex = 99;
+    box2.style.zIndex = 98;
+    box3.style.zIndex = 97;
+
+    var init_top = 150;
+    var init_left = width/2 - 400;
+
+
+    box1.style.top = String(init_top - 20) + "px";
+    box1.style.left = String(init_left - 20) + "px";
+    box2.style.top = String(init_top - 40) + "px";
+    box2.style.left = String(init_left - 40) + "px";
+    box3.style.top = String(init_top - 60) + "px";
+    box3.style.left = String(init_left - 60) + "px";
+    box.style.top = String(init_top) + "px";
+    box.style.left = String(init_left) + "px";
+
+};
+
+$(window).resize(onsize);
+onsize();
 // box4.style.left =  String(init_left) + "px";
 // box5.style.left =  String(init_left) + "px";
 
